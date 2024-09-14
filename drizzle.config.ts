4ccb9953d6f2aa39c565c10cb074e4c576a3aad7
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2024-09-14 00:03:04 -0300

    Improve drizzle setup

---
+++ drizzle.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./app/.server/db/schema.ts",
+  out: "./drizzle",
+  dialect: "sqlite",
+  verbose: true,
+});
